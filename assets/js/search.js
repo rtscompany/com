fa31147d59cc32583d107098b1734d41fdd2f9e9
rtscompany <redotpay827@gmail.com>
2025-08-09 16:45:47 +0600

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -19,96 +19,14 @@ function searchKeyword() {
     // Redirect to a separate page based on the keyword
     if (keyword === "eh0215336") {
         window.location.href = "bulbul.html";
-    }else if (keyword === "a03076610") {
-        window.location.href = "niloy.html";
-    }else if (keyword === "a14817555") {
-        window.location.href = "sahid.html";
-    }else if (keyword === "a07942774") {
-        window.location.href = "rowshan.html";
-    }else if (keyword === "a17002294") {
-        window.location.href = "akter.html";
-    }else if (keyword === "b00801317") {
-        window.location.href = "Ala.html";
-    }else if (keyword === "a17002295") {
-        window.location.href = "hasen.html";
-    }else if (keyword === "a07437621") {
-        window.location.href = "saidul.html";
-    }else if (keyword === "a17366368") {
-        window.location.href = "rasida.html";
+    }else if (keyword === "a12586368") {
+        window.location.href = "Sref.html";
     }else if (keyword === "ek0483061") {
         window.location.href = "sohal.html";
     }else if (keyword === "a17231095") {
         window.location.href = "morshed.html";
-    }else if (keyword === "a05632793") {
-        window.location.href = "saiful.html";
-    }else if (keyword === "a11611142") {
-        window.location.href = "sabuz.html";
-    }else if (keyword === "a12886582") {
-        window.location.href = "iqbal.html";
     }else if (keyword === "em0449880") {
         window.location.href = "amin.html";
-    }else if (keyword === "bw0504380") {
-        window.location.href = "abadur.html";
-    }else if (keyword === "eg012345") {
-        window.location.href = "nadimA.html";
-    }else if (keyword === "nadimJ") {
-        window.location.href = "nadimJ.html";
-    }else if (keyword === "nadimW") {
-        window.location.href = "nadimW.html";
-    }else if (keyword === "eg0940141") {
-        window.location.href = "nadim.html";
-    }else if (keyword === "ej0169453") {
-        window.location.href = "https://canadaworkspot.com/azizur/";
-    } else if (keyword === "ej0021978") {
-        window.location.href = "https://canadacheack.github.io/Visacheak/sohel.html";
-    } else if (keyword === "a01318356") {
-        window.location.href = "https://australiaworkspot.github.io/com/murad.html";
-    } else if (keyword === "a08392536") {
-        window.location.href = "https://canadacheack.github.io/Visacheak/IMAN.html";
-    } else if (keyword === "ek0983359") {
-        window.location.href = "https://australiavisa.github.io/cheak/azizul.html";
-    } else if (keyword === "el051924") {
-        window.location.href = "https://canadacheack.github.io/Visacheak/saik.html";
-    } else if (keyword === "a04282610") {
-        window.location.href = "https://australiavisa.github.io/cheak/rakib.html";
-    } else if (keyword === "el0374736") {
-        window.location.href = "https://australiavisa.github.io/cheak/akramul.html";
-    } else if (keyword === "b00237385") {
-        window.location.href = "https://canadacheack.github.io/Visacheak/jahid.html";    
-    } else if (keyword === "el0824465") {
-        window.location.href = "https://canadacheack.github.io/Visacheak/MOHAMMED.html";
-    } else if (keyword === "u5467929") {
-        window.location.href = "https://australiavisa.github.io/cheak/probir.html";
-    } else if (keyword === "a03934916") {
-        window.location.href = "https://canadacheack.github.io/Visacheak/Julhas.html";
-    } else if (keyword === "eh0733415") {
-        window.location.href = "https://canadacheack.github.io/Visacheak/moshiar.html";
-    }  else if (keyword === "eh0733416") {
-        window.location.href = "https://canadacheack.github.io/Visacheak/moshiar.html";
-    } else if (keyword === "eh0642768") {
-        window.location.href = "https://canadaworkspot.com/sabbir/";
-    } else if (keyword === "el0416811") {
-        window.location.href = "https://canadaworkspot.com/Sorif_Ahmod/";
-    } else if (keyword === "eg0751679") {
-        window.location.href = "https://canadaworkspot.com/Kamal/";
-    } else if (keyword === "bq0326331") {
-        window.location.href = "https://canadaworkspot.com/Maiz-Uddin/";
-    } else if (keyword === "eg0314183") {
-        window.location.href = "https://canadaworkspot.com/mamun/";
-    } else if (keyword === "ej0962025") {
-        window.location.href = "https://canadaworkspot.com/Ekal_Khandakar/";
-    } else if (keyword === "a02356991") {
-        window.location.href = "https://canadaworkspot.com/foysol_ahmed/";
-    } else if (keyword === "el0320583") {
-        window.location.href = "https://canadaworkspot.com/Alamgir_Bhuiyan/";
-    } else if (keyword === "ek0656546") {
-        window.location.href = "https://canadaworkspot.com/Raihan_Hossain/";
-    } else if (keyword === "a13435184") {
-        window.location.href = "https://canadaworkspot.com/Monsur-Rahman/";
-    } else if (keyword === "ej09620225") {
-        window.location.href = "https://canadaworkspot.com/Ekbal/";
-    } else if (keyword === "ej0354747") {
-        window.location.href = "https://canadaworkspot.com/Mazaharul/";
     } else if (keyword === "") {
         window.location.href = "";
     } else if (keyword === "") {
